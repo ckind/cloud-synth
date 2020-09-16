d663d8e81d7ec5d93f769fcd17496351df509531
ckind <carlo.a.kind@gmail.com>
2020-09-16 15:05:21 +0400

    Update JvaSynth.vue
    adjusted range for pitch mod

--- src/components/JvaSynth.vue
+++ src/components/JvaSynth.vue
@@ -270,7 +270,7 @@
             <knob-control
               v-model="settings.pitch.modulationAmount"
               :minValue="0"
-              :maxValue="0.1"
+              :maxValue="0.01"
               id="pitchModKnob"
               label="Pitch Mod"
             ></knob-control>
