24062622f3831c7b0adce271e58f69d9dc393996
Carlo Kind <carlo.a.kind@gmail.com>
2021-05-11 14:35:50 +0400

    adjusted css in knob control for more browser support

--- src/components/KnobControl.vue
+++ src/components/KnobControl.vue
@@ -185,6 +185,9 @@ export default class KnobControl extends Vue {
   display: flex;
 }
 .knob-img {
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  -khtml-border-radius: 50%;
   border-radius: 50%;
   user-select: none;
 }
